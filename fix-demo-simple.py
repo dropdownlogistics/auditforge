532170cf67cd50682e55297b6c0903efc6c6e5f2
DropDownLogistics <dropdownlogistics@gmail.com>
2026-03-14 16:40:14 -0500

    simplify demo route to sign-in redirect

---
+++ fix-demo-simple.py
@@ -0,0 +1,8 @@
+﻿content = '''import { NextResponse } from 'next/server'
+
+export async function GET(request) {
+  return NextResponse.redirect(new URL('/sign-in', request.url))
+}
+'''
+open('src/app/api/auth/demo/route.js', 'w', encoding='utf-8').write(content)
+print('done')
